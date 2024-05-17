13dd9fea633f166fba148e150680b7a03d6f7b33
Victor Osório Ramos Porto <victor.porto@mpse.mp.br>
2024-05-17 11:14:06 -0300

    Parametrizando chamadas

--- Server3/main.py
+++ Server3/main.py
@@ -2,10 +2,16 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
-def hello():
+server_number = 3
+
+@app.route("/api", methods=["GET"])
+def functionApi():
+    return jsonify({"message": f"API do servidor {server_number}"})
+
+@app.route("/api/welcome", methods=["GET"])
+def functionWelcome():
     ip_address = request.headers.get("X-Forwarded-For")
-    return jsonify({"message": "Bem vindo ao servidor 3", "ip_address": ip_address})
+    return jsonify({"message": f"Bem vindo ao servidor {server_number}", "ip_address": ip_address})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=False)
